4d80b54370db0f3d4f9b5a29f775c4bf5178f0ed
Adriano Ament <adrianojament@gmail.com>
2019-08-15 23:44:28 -0300

    Novas implementacoes

---
+++ frontend/src/dashboard/dashboard.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+
+import ContentHeader from '../common/template/contentheader'
+import Content from '../common/template/content'
+import Row from '../common/layout/row'
+import ValueBox from '../common/widget/valueBox'
+
+class Dashboard extends Component{
+    render() {
+        return (
+            <div >
+                <ContentHeader title='Dashboard' small='Versao 1.0' />
+                <Content>
+                    <Row>
+                        <ValueBox 
+                            cols='12 4' 
+                            color='green' 
+                            icon='bank' 
+                            value='R$ 10.00'
+                            text= 'Total de Créditos'/>     
+                        <ValueBox 
+                            cols='12 4' 
+                            color='red' 
+                            icon='credit-card' 
+                            value='R$ 10.00'
+                            text= 'Total de Débitos'/>     
+                        <ValueBox 
+                            cols='12 4' 
+                            color='blue' 
+                            icon='money' 
+                            value='R$ 0.00'
+                            text= 'Total Consolidado'/>     
+                    </Row>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
